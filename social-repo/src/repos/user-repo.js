75486ed5c754a3981d2353229942066968f220d4
Nataliia Hirniak <gn.natalia19@gmail.com>
2023-05-06 10:52:03 +0100

    Add find functionality

---
+++ social-repo/src/repos/user-repo.js
@@ -0,0 +1,20 @@
+const pool = require("../pool");
+const toCamelCase = require("./helpers/to-camel-case");
+
+class UserRepo {
+  static async find() {
+    const { rows } = await pool.query("SELECT * FROM users;");
+
+    return toCamelCase(rows);
+  }
+
+  static async findById() {}
+
+  static async insert() {}
+
+  static async update() {}
+
+  static async delete() {}
+}
+
+module.exports = UserRepo;
