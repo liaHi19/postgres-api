d8fb9a637925633267fde7df5ef91957555d2300
Nataliia Hirniak <gn.natalia19@gmail.com>
2023-05-06 11:23:28 +0100

    Add findById functionality

--- social-repo/src/repos/user-repo.js
+++ social-repo/src/repos/user-repo.js
@@ -8,7 +8,13 @@ class UserRepo {
     return toCamelCase(rows);
   }
 
-  static async findById() {}
+  static async findById(id) {
+    const { rows } = await pool.query(`SELECT * FROM users WHERE id = $1;`, [
+      id,
+    ]);
+
+    return toCamelCase(rows)[0];
+  }
 
   static async insert() {}
 
--- social-repo/src/routes/users.js
+++ social-repo/src/routes/users.js
@@ -10,7 +10,17 @@ router.get("/users", async (req, res) => {
   res.send(users);
 });
 
-router.get("/users/:id", async (req, res) => {});
+router.get("/users/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const user = await UserRepo.findById(id);
+
+  if (user) {
+    res.send(user);
+  } else {
+    res.status(404).json({ message: "Can't find user" });
+  }
+});
 
 router.post("/users", async (req, res) => {});
 
